63330f95e8f9742438d050edb5d57af45bd4109a
hisho <hisho.web@gmail.com>
2021-05-24 23:58:06 +0900

    ✨ add template page

---
+++ src/pages/template/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {renderToStaticMarkup} from "@src/configs";
+import {Layout, SEO} from "@src/layouts";
+import {Picture} from "@src/components";
+
+export default () => {
+  return renderToStaticMarkup(
+    <Layout page_id="0">
+      <SEO/>
+
+      <article>
+        <h2>Picture Component</h2>
+        <Picture src="dummy/screenshot.png" />
+        <Picture src="common/ogp.png" />
+      </article>
+
+    </Layout>
+  );
+};
